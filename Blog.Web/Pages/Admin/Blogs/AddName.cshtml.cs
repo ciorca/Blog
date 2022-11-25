dae58e6d89a8e0c42c4ae464a717384f5c2ec686
ciorca <ciorca.gaby@gmail.com>
2022-11-25 15:59:28 +0200

    Layout modified and Admin navbar added.Database created and migration created.

---
+++ Blog.Web/Pages/Admin/Blogs/AddName.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Web.Pages.Admin.Blogs
+{
+    public class AddNameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
