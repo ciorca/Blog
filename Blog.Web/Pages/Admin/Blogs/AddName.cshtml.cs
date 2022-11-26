474f04a2fca3327b9540e4d914cc94e832c10fa8
ciorca <ciorca.gaby@gmail.com>
2022-11-26 20:21:19 +0200

    Created a new class,compilated de code behind the page AddName.cshtml.cs and created the form for the page AddName.cshtml

--- Blog.Web/Pages/Admin/Blogs/AddName.cshtml.cs
+++ Blog.Web/Pages/Admin/Blogs/AddName.cshtml.cs
@@ -1,3 +1,6 @@
+using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,36 @@ namespace Blog.Web.Pages.Admin.Blogs
 {
     public class AddNameModel : PageModel
     {
+        private readonly BlogDbContext blogDbContext;
+
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
+        public AddNameModel(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
         public void OnGet()
         {
         }
+        public void OnPost()
+        {
+            var blogPost = new BlogPost() 
+            {
+            Heading=AddBlogPostRequest.Heading,
+            PageTitle=AddBlogPostRequest.PageTitle,
+            Content=AddBlogPostRequest.Content,
+            ShortDescription=AddBlogPostRequest.ShortDescription,
+            FeaturedImageUrl=AddBlogPostRequest.FeaturedImageUrl,
+            PublishedDate=AddBlogPostRequest.PublishedDate,
+            Author=AddBlogPostRequest.Author,
+            Visible=AddBlogPostRequest.Visible
+            };
+
+            blogDbContext.BlogPosts.Add(blogPost);
+            blogDbContext.SaveChanges();
+        }
+
+
     }
 }
